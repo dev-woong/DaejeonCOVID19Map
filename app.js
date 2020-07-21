92c3bb6fbf59b0d3a7ac5def195d98d5631a36cd
dev-woong <dev-woong>
2020-07-22 08:46:20 +0900

    add marker event when selecting people

--- app.js
+++ app.js
@@ -17,10 +17,9 @@ let jsonData = $.getJSON(dirJson, function (data) {
 let container = document.getElementById("map")
 // 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
 let infowindow = new kakao.maps.InfoWindow({ zIndex: 1 })
-let options = {
-  center: new kakao.maps.LatLng(36.350439, 127.384851),
-  level: 3,
-}
+
+let markerList = new Map()
+let circleList = new Map()
 
 let userOptions = {
   circleSize: 200,
@@ -28,76 +27,30 @@ let userOptions = {
     red: "#ef5350",
     orenge: "#ffa726",
     blue: "#42a5f5",
+    green: "#66bb6a",
   },
 }
 
+let arrPeopleList = []
+
 document.getElementById("map").style.height = window.innerHeight - 20 + "px"
 //맵 생성
+let options = {
+  center: new kakao.maps.LatLng(36.350439, 127.384851),
+  level: 5,
+}
 let map = new kakao.maps.Map(container, options)
 
-// 주소 검색
-document.getElementById("btnSearch").onclick = function () {
-  let txtSearch = document.getElementById("txtSearch").value
-  if (txtSearch === "") {
-    alert("검색어를 입력해주세요.")
-    return false
-  }
-
-  //키워드 대전으로 한정
-  txtSearch = "대전 " + txtSearch
-
-  // 장소 검색 객체를 생성합니다
-  let ps = new kakao.maps.services.Places()
-
-  // 키워드로 장소를 검색합니다
-  ps.keywordSearch(txtSearch, placesSearchCB)
-
-  // 키워드 검색 완료 시 호출되는 콜백함수 입니다
-  function placesSearchCB(data, status, pagination) {
-    if (status === kakao.maps.services.Status.OK) {
-      // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-      // LatLngBounds 객체에 좌표를 추가합니다
-      let bounds = new kakao.maps.LatLngBounds()
-
-      for (let i = 0; i < data.length; i++) {
-        displayMarker(data[i])
-        bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x))
-      }
-
-      // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-      map.setBounds(bounds)
-    }
-  }
-
-  // 지도에 마커를 표시하는 함수입니다
-  function displayMarker(place) {
-    // 마커를 생성하고 지도에 표시합니다
-    let marker = new kakao.maps.Marker({
-      map: map,
-      position: new kakao.maps.LatLng(place.y, place.x),
-    })
-
-    // 마커에 클릭이벤트를 등록합니다
-    kakao.maps.event.addListener(marker, "click", function () {
-      // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-      infowindow.setContent(
-        '<div style="padding:5px;font-size:12px;">' + place.place_name + "</div>"
-      )
-      infowindow.open(map, marker)
-    })
-  }
-}
-
+// 메뉴 숨김 버튼 클릭 시
 $("#hideMenu__checkbox").change(function () {
   if ($("#hideMenu__checkbox").is(":checked")) {
-    $("#search__checkbox").prop("checked", true)
+    // $("#search__checkbox").prop("checked", true)
     if (window.innerWidth > 768) {
       $(".menu").css("-webkit-animation-name", "bicScreenHideMenu")
     } else {
       $(".menu").css("-webkit-animation-name", "smallScreenHideMenu")
     }
   } else {
-    //   $(".s").css("-webkit-animation-name", "reverseDuration")
     $(".menu").css("-webkit-animation-name", "showMenu")
     if (window.innerWidth > 768) {
       $(".menu").css("-webkit-animation-name", "bicScreenShowMenu")
@@ -106,24 +59,43 @@ $("#hideMenu__checkbox").change(function () {
     }
   }
 })
-// document.getElementsByClassName("people__content")[0].onclick = function () {}
 
 $(function () {
   let data = jsonData.responseJSON
   let reverseData = data.reverse()
 
+  $("#people__checkbox").prop("checked", true)
+
   // 주소-좌표 변환 객체를 생성합니다
   let geocoder = new kakao.maps.services.Geocoder()
 
+  //전체 선택 li element 및 event 추가
+  let li = document.createElement("li")
+  li.innerHTML = "전체 선택"
+  li.className = "li__0"
+  li.id = "li__0"
+  li.value = 0
+  document.getElementsByClassName("people__content")[0].append(li)
+  let liId = document.getElementById("li__0")
+  liId.addEventListener("click", clickPeopleContent, false)
+
+  //확진자 li element 및 event 추가
   for (let i in reverseData) {
-    let elem = document.createElement("li")
-    elem.innerHTML = data[i].confirmer + "번 " + data[i].place
-    document.getElementsByClassName("people__content")[0].append(elem)
+    arrPeopleList.push(data[i].confirmer)
+    let li = document.createElement("li")
+    li.innerHTML = data[i].confirmer + "번 " + data[i].place
+    li.className = "li__" + data[i].confirmer
+    li.id = "li__" + data[i].confirmer
+    li.value = 0
+    document.getElementsByClassName("people__content")[0].append(li)
+    let liId = document.getElementById("li__" + data[i].confirmer)
+    liId.addEventListener("click", clickPeopleContent, false)
   }
+
   //확진자 번호
   for (let i in data) {
     for (let j in data[i].route) {
-      if (getPastTime(data[i].route[j].dateOfExposure[length + 1]) > 14) return false
+      if (getPastTime(data[i].route[j].dateOfExposure[length + 1]) > 30) return false
       let circleColor = getCircleColor(data[i].route[j].dateOfExposure[length + 1])
       // 주소로 좌표를 검색합니다
       geocoder.addressSearch("대전" + data[i].route[j].location, function (result, status) {
@@ -150,6 +122,7 @@ $(function () {
                 "</div>"
             )
             infowindow.open(map, marker)
+            // infowindow.close()
           })
 
           // 지도에 표시할 원을 생성합니다
@@ -158,13 +131,18 @@ $(function () {
             radius: userOptions.circleSize, // 미터 단위의 원의 반지름입니다
             strokeOpacity: 0, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
             fillColor: circleColor, // 채우기 색깔입니다
-            fillOpacity: 0.5, // 채우기 불투명도 입니다
+            fillOpacity: 0.4, // 채우기 불투명도 입니다
           })
 
           // 지도에 원을 표시합니다
           circle.setMap(map)
           // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-          map.setCenter(coords)
+          // map.setCenter(coords)
+          markerList.set(data[i].confirmer + "_" + j, marker)
+          marker.setVisible(false)
+
+          circleList.set(data[i].confirmer + "_" + j, circle)
+          circle.setMap(null)
         }
       })
     }
--- func.js
+++ func.js
@@ -8,15 +8,73 @@ let getCircleColor = (data) => {
     return userOptions.circleColor.red
   } else if (now - date > day * 2 && now - date < day * 7) {
     return userOptions.circleColor.orenge
-  } else {
+  } else if (now - date > day * 7 && now - date < day * 14) {
     return userOptions.circleColor.blue
+  } else {
+    return userOptions.circleColor.green
   }
 }
-
+//데이터를 받아 현재 시간 기준으로 얼마나 지났는지 반환
 let getPastTime = (data) => {
   let date = new Date(data)
   let now = new Date()
   let day = 86400000
 
   return (now - date) / day
+}
+
+//확진자 정보를 눌렀을 때
+function clickPeopleContent() {
+  let id = this.id
+  let value = this.value
+  let num = id.substring(4)
+
+  if (id === "li__0") {
+    for (let i in arrPeopleList) {
+      $("#li__" + arrPeopleList[i]).prop("value", value)
+      $("#li__" + arrPeopleList[i]).trigger("click")
+    }
+    if (value % 2 === 0) {
+      let sw = new kakao.maps.LatLng(36.291746, 127.318748),
+        ne = new kakao.maps.LatLng(36.408766, 127.42385)
+
+      let bounds = new kakao.maps.LatLngBounds(sw, ne) // 인자를 주지 않으면 빈 영역을 생성한다.
+      map.panTo(bounds)
+    }
+  }
+
+  if (value % 2 === 1) {
+    $("#" + this.id).css("background-color", "white")
+    if (num !== "0") {
+      let i = 0
+      while (i < 10) {
+        if (markerList.has(num + "_" + i)) {
+          markerList.get(num + "_" + i).setVisible(false)
+          circleList.get(num + "_" + i).setMap(null)
+          infowindow.close()
+        } else {
+          break
+        }
+        i++
+      }
+    }
+  } else {
+    $("#" + this.id).css("background-color", " #ffbb93")
+    if (num !== "0") {
+      let i = 0
+      while (i < 10) {
+        if (markerList.has(num + "_" + i)) {
+          markerList.get(num + "_" + i).setVisible(true)
+          circleList.get(num + "_" + i).setMap(map)
+          map.panTo(markerList.get(num + "_" + i).getPosition())
+        } else {
+          break
+        }
+        i++
+      }
+    }
+  }
+
+  value++
+  $("#" + this.id).prop("value", value)
 }
